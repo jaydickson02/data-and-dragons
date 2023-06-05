cedd53519858beb0e7e2b5760faac76f944d67c1
Jay Dickson <jaydickson02@gmail.com>
2023-06-05 11:56:19 +1000

    Fixed loading bar spinner

--- src/components/loading.js
+++ src/components/loading.js
@@ -9,7 +9,7 @@ export default function Example() {
             <p className="text-base font-semibold text-indigo-600">200</p>
             <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Loading</h1>
             <p className="mt-6 text-base leading-7 text-gray-600">We are getting everything ready!</p>
-            <div class="flex items-center">
+            <div class="flex items-center justify-center mt-10">
                 <FaSpinner class ="fa-spin" />
             </div>
             <div className="mt-10 flex items-center justify-center gap-x-6">
