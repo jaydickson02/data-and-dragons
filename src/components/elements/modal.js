c92a93004d5f2ceb1bd7bc6a38bbe4034ec47660
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2024-08-05 00:56:00 +1000

    Update modal.js

--- src/components/elements/modal.js
+++ src/components/elements/modal.js
@@ -6,7 +6,7 @@ const Modal = ({ children, onClose }) => {
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
             <div className="relative bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-300 p-6 rounded-lg shadow-lg w-11/12 sm:w-1/2 max-h-[calc(100%-4rem)] overflow-auto mt-8 mb-8">
                 <button
-                    className="fixed top-8 right-8 text-gray-700 dark:text-gray-300 bg-gray-200 dark:bg-gray-700 p-2 rounded-lg"
+                    className="fixed top-12 right-8 text-gray-700 dark:text-gray-300 bg-gray-200 dark:bg-gray-700 p-2 rounded-lg"
                     onClick={onClose}
                 >
                     <FaTimes size={20} />
