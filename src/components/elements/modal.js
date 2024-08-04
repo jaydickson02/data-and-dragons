98e8412562cb9eb88f642e51a7e98a1332670cb0
Jay Dickson <21349032+jaydickson02@users.noreply.github.com>
2024-08-05 00:22:44 +1000

    Fixed modal display issues

--- src/components/elements/modal.js
+++ src/components/elements/modal.js
@@ -4,9 +4,9 @@ import { FaTimes } from 'react-icons/fa';
 const Modal = ({ children, onClose }) => {
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-            <div className="bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-300 p-6 rounded-lg shadow-lg w-11/12 sm:w-1/2 relative">
+            <div className="relative bg-gray-100 dark:bg-gray-800 text-gray-700 dark:text-gray-300 p-6 rounded-lg shadow-lg w-11/12 sm:w-1/2 max-h-[calc(100%-4rem)] overflow-auto mt-8 mb-8">
                 <button
-                    className="absolute top-2 right-2 text-gray-700 dark:text-gray-300"
+                    className="fixed top-8 right-8 text-gray-700 dark:text-gray-300 bg-gray-200 dark:bg-gray-700 p-2 rounded-lg"
                     onClick={onClose}
                 >
                     <FaTimes size={20} />
